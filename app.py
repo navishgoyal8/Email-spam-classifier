059ecb535235abc7a4f5425a5d9cac346468d9a0
Navish <navishgoyal50@gmail.com>
2024-07-11 16:55:29 +0530

    nltk sort

--- app.py
+++ app.py
@@ -2,8 +2,13 @@ import streamlit as st
 import pickle
 import string
 import nltk
+
+nltk.download('punkt')
+
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
+
+
 
 ps = PorterStemmer()
 
